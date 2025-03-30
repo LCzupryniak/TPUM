21150814ef34d50da8413b0ad2f326628c79a929
LCzupryniak <l.czupryniak01@gmail.com>
2025-03-31 00:27:32 +0200

    fix

--- ShopApp/Presentation/ViewModels/MainViewModel.cs
+++ ShopApp/Presentation/ViewModels/MainViewModel.cs
@@ -7,7 +7,6 @@ using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Input;
-using Logic.Interfaces;
 
 namespace Presentation.ViewModels
 {
