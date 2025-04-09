c92ca09c7d7d1b7141739a0d3e043789aa7944bb
LCzupryniak <l.czupryniak01@gmail.com>
2025-04-09 23:02:32 +0200

    GUI fix

--- View/MainWindow.xaml.cs
+++ View/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using Model;
 using ViewModel;
 using Logic.API;
+using System.Text.RegularExpressions;
 
 namespace View
 {
@@ -14,10 +15,33 @@ namespace View
             var shopService = factory.CreateShopService();
             var stockNotifier = factory.CreateStockNotifier();
 
-            
+
             var logicModelBridge = new LogicModelBridge(shopService, stockNotifier);
 
             DataContext = new MainViewModel(logicModelBridge, logicModelBridge);
+        }
+        private void NumberValidationTextBox(object sender, System.Windows.Input.TextCompositionEventArgs e)
+        {
+            Regex regex = new Regex("[^0-9]+");
+            e.Handled = regex.IsMatch(e.Text);
+        }
+
+        private void IncreaseQuantity_Click(object sender, RoutedEventArgs e)
+        {
+            var context = ((FrameworkElement)sender).DataContext as ProductViewModel;
+            if (context != null)
+            {
+                context.SelectedQuantity++;
+            }
+        }
+
+        private void DecreaseQuantity_Click(object sender, RoutedEventArgs e)
+        {
+            var context = ((FrameworkElement)sender).DataContext as ProductViewModel;
+            if (context != null && context.SelectedQuantity > 0)
+            {
+                context.SelectedQuantity--;
+            }
         }
     }
 }
